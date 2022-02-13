c504770a131dae89f0abfbfb7aa6b3a1524af01e
Luis Arevalo <luisgabrielarevalo@hotmail.com>
2022-02-13 14:07:00 -0500

    add search Component

--- src/components/SearchComponent.tsx
+++ src/components/SearchComponent.tsx
@@ -2,7 +2,18 @@
 
 const SearchComponent = () => {
   return (
-    <div>SearchComponent</div>
+    <div className="search">
+        <h1 className="search__title">IP Address Tracker</h1>
+        <div className="search__input-field">
+            <input 
+                placeholder="Search for any IP address or domain"
+                className="search__input"
+            />
+            <div className="search__icon-field">
+                <img src='assets/icon-arrow.svg' alt="arrow"/>  
+            </div>
+        </div>
+    </div>
   )
 }
 
