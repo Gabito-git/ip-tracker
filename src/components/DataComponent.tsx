196e3d3a13dbdca8b2424cabf1267585832e45df
Luis Arevalo <luisgabrielarevalo@hotmail.com>
2022-02-16 17:21:52 -0500

    add correct structure to dataComponent

--- src/components/DataComponent.tsx
+++ src/components/DataComponent.tsx
@@ -1,7 +1,27 @@
 
 const DataComponent = () => {
   return (
-    <div>DataComponent</div>
+    <div className="data">
+      <div className="data__group">
+        <p className="data__fieldname">IP ADDRESS</p>
+        <p className="data__fieldvalue">192.212.174.101</p>
+      </div>
+
+      <div className="data__group">
+        <p className="data__fieldname">LOCATION</p>
+        <p className="data__fieldvalue">Brooklyn, NY 10001</p>
+      </div>
+
+      <div className="data__group">
+        <p className="data__fieldname">TIMEZONE</p>
+        <p className="data__fieldvalue">UTC -05:00</p>
+      </div>
+
+      <div className="data__group">
+        <p className="data__fieldname">ISP</p>
+        <p className="data__fieldvalue">SpaceX Starlink</p>
+      </div>
+    </div>
   )
 }
 
