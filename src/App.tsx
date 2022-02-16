fe408c6e9898fd66fb31372274e80e753a1f7bcb
Luis Arevalo <luisgabrielarevalo@hotmail.com>
2022-02-16 18:16:14 -0500

    add components to root component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import DataComponent from './components/DataComponent';
+import MapComponent from './components/MapComponent';
+import SearchComponent from './components/SearchComponent';
 import './styles/index.scss';
 
 function App() {
   return (
     <div className="App">
-        
+        <SearchComponent />
+        <DataComponent />
+        <MapComponent />
     </div>
   );
 }
